5deb1f1279defa35e74c72ea44911c32f1979b4f
vidushi vyas <vidushivyas01@gmail.com>
2026-01-28 22:57:23 +0530

    Updated Project 9

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-(--c2'>
+      <h1>Home</h1>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import logo from "../assets/logo.png";
 const Nav = () => {
+
+const [open, setOpen] = useState(false)
+
   return (
-    <div className="">
+    <div className="bg-(--c1)  flex justify-between items-center p-4 text-black font-bold">
+     <img src={logo} className="w-10 h-10 object-cover" />
+      <p className="font-bold text-2xl text-black">My Recipies</p>
+      <button 
+      className="md:hidden text-2xl "
+      onClick={ () =>
+        setOpen(true)
+      }>☰</button>
+      {open && (
+        <div
+        className="fixed inset-0 bg-black/40"
+          onClick={() =>
+            setOpen(false)}>
+        </div>
+
+      )}
         <Link to="/">Home</Link>
          <Link to="/product">Product</Link>
           <Link to="/service">Service</Link>
            <Link to="/about">About</Link>
+           
     </div>
   )
 }
